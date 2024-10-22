3afce4856287de55733c7292c365d97116f42396
evggas <evgeniyagas2000@gmail.com>
2024-10-22 22:27:54 +0300

    price no

---
+++ divan_spider/divan_spider/spiders/lighting.py
@@ -0,0 +1,22 @@
+import scrapy
+
+class DivanSpider(scrapy.Spider):
+    name = "divan_spider"
+    allowed_domains = ["divan.ru"]
+    start_urls = ['https://www.divan.ru/category/svet']
+
+    def parse(self, response):
+        products = response.css("div._Ud0k")
+
+        for product in products:
+            yield {
+                'name': product.css("div.lsooF span::text").get(),
+                'price': product.css("div.pY3d2::text").get(),
+                "url": product.css("a").attrib ["href"]
+            }
+
+        # Пагинация, если есть ссылки на следующие страницы
+        next_page = response.css('a.pagination-next::attr(href)').get()
+        if next_page is not None:
+           yield response.follow(next_page, self.parse)
+#scrapy crawl divan_spider -o lights.json
